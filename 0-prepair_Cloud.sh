7f22ef01942302ff33859bfa278264026c35202b
luongvinhthao <luongvinhthao@gmail.com>
2014-07-22 11:50:20 +0700

    Signed-off-by: luongvinhthao <luongvinhthao@gmail.com>

--- 0-prepair_Cloud.sh
+++ 0-prepair_Cloud.sh
@@ -1,30 +1,27 @@
 #!/usr/bin/bash 
 source config.cfg
+
+
+echo "---------------------------- cap nhat cac goi cho ubuntu ----------------------------"
+apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
 apt-get install -y python-software-properties
 add-apt-repository cloud-archive:havana
-
-echo "########## cap nhat cac goi cho ubuntu ##########"
-apt-get update && apt-get -y upgrade && apt-get -y dist-upgrade 
-
-
-
-
-echo "########## Khai bao Hostname cho ubuntu ##########"
-
+echo "---------------------------- Khai bao Hostname cho ubuntu ----------------------------"
+sleep 2
 hostname controller
 echo "controller" > /etc/hostname
 
-echo "########## Cai dat & cau hinh NTP ##########"
+echo "---------------------------- Cai dat & cau hinh NTP ----------------------------"
 apt-get install -y ntp
-echo "########## Khoi dong lai NTP ##########"
+echo "---------------------------- Khoi dong lai NTP ----------------------------"
 sleep 3
 service ntp restart
 
 
-echo "########## install Messaging server cho ubuntu ##########"
+echo "---------------------------- cai dat Messaging server cho ubuntu ----------------------------"
 apt-get install -y rabbitmq-server
 rabbitmqctl change_password guest $RABBIT_PASS
-echo "########## Khoi dong lai may ##########"
+echo "---------------------------- Khoi dong lai may ----------------------------"
 sleep 2
 service rabbitmq-server restart
 sleep 2
